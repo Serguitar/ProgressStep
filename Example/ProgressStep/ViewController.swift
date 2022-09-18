4fe3dbf2f940fe6e92e8a4fb93b8084401692315
Sergei Lukoianov <serguitar@mail.ru>
2022-09-18 22:15:18 +0400

    clean up example app

--- Example/ProgressStep/ViewController.swift
+++ Example/ProgressStep/ViewController.swift
@@ -16,18 +16,6 @@ class ViewController: UIViewController {
     
     var currentValue: CGFloat = 0.0
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
     @IBAction func addTapped(_ sender: Any) {
         currentValue += 0.5
         valueTF.text = "\(currentValue)"
